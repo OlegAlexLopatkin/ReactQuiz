4c6485d279059876ba5fa355d3094e49564470de
Oleg Lopatkin <redoubt1@mail.ru>
2020-03-04 16:17:04 +0600

    Begin

---
+++ src/store/actions/auth.js
@@ -0,0 +1,83 @@
+import axios from "axios";
+import { AUTH_SUCCESS, AUTH_LOGOUT } from "./actionType";
+import key from '../../keys'
+
+let timeout;
+
+export function auth(email, password, isLogin) {
+  return async dispatch => {
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+
+    let url =
+      `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${key}`;
+
+    if (isLogin) {
+      url =
+        `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${key}`;
+    }
+
+    const response = await axios.post(url, authData);
+    const data = response.data;
+
+    const expirationDate = new Date(
+      new Date().getTime() + data.expiresIn * 1000
+    );
+
+    localStorage.setItem("token", data.idToken);
+    localStorage.setItem("userId", data.localId);
+    localStorage.setItem("expirationDate", expirationDate);
+
+    dispatch(authSuccess(data.idToken, data.localId));
+    dispatch(autoLogout(data.expiresIn));
+  };
+}
+
+export function autoLogout(time) {
+  return dispatch => {
+    timeout = setTimeout(() => {
+      dispatch(logout());
+    }, time * 1000);
+  };
+}
+
+export function logout() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("expirationDate");
+  clearTimeout(timeout);
+  return {
+    type: AUTH_LOGOUT
+  };
+}
+
+export function autoLogin() {
+  return dispatch => {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem("expirationDate"));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        dispatch(authSuccess(token, userId));
+        dispatch(
+          autoLogout((expirationDate.getTime() - new Date().getTime()) / 1000)
+        );
+      }
+    }
+  };
+}
+
+export function authSuccess(token, userId) {
+  return {
+    type: AUTH_SUCCESS,
+    token,
+    userId
+  };
+}
